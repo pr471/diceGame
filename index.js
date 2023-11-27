0cf5415a86da7cf464b9edc99c3cdcc7a0f4b059
pouya <pouyarashidi471@gmail.com>
2023-11-27 16:07:14 +0330

    Dice Gamne with Refresh - first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) +1;
+var randomeDiceImage = "images/dice"+randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src" ,randomeDiceImage );
+
+var randomNumber2 = Math.floor(Math.random() *6) +1;
+var randomDiceImage2 = "images/dice"+randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src" , randomDiceImage2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerText = "Player 1 wins!"
+}
+else if (randomNumber1<randomNumber2){
+    document.querySelector("h1").innerText = "Player 2 wins!"
+}
+else {
+    document.querySelector("h1").innerText = "Draw!"
+}
